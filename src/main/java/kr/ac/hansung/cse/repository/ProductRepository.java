27ebc35af747189df1ef5231b330e7bf97314640
Lim Li Bin <dst703410@gmail.com>
2026-04-13 20:30:34 +0900

    fix: 검색 결과 출력 시 푸터 영역 누락 오류 수정

--- src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
+++ src/main/java/kr/ac/hansung/cse/repository/ProductRepository.java
@@ -136,7 +136,7 @@ public class ProductRepository {
     //이름 검색
     public List<Product> findByNameContaining(String keyword) {
         return entityManager.createQuery(
-                "SELECT p FROM Product p WHERE p.name LIKE :keyword",
+                        "SELECT p FROM Product p LEFT JOIN FETCH p.category WHERE p.name LIKE :keyword",
                 Product.class)
                 .setParameter("keyword", "%" + keyword + "%")
                 .getResultList();
@@ -145,7 +145,7 @@ public class ProductRepository {
     //카테고리 필터
     public List<Product> findByCategoryId(Long categoryId) {
         return entityManager.createQuery(
-                "SELECT p FROM Product p WHERE p.category.id = :cid",
+                        "SELECT p FROM Product p LEFT JOIN FETCH p.category WHERE p.category.id = :cid",
                 Product.class)
                 .setParameter("cid", categoryId)
                 .getResultList();
